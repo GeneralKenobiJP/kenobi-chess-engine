db2c3b0f4b35fe0d25bb78d173a26abc5d352ad8
Jakub Pietrzak <kubapietrzak7@gmail.com>
2024-08-05 20:58:37 +0200

    Expanded UCI handling

---
+++ src/board.rs
@@ -0,0 +1,5 @@
+const START_POSITION: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0";
+
+pub fn read_fen(fen: &str) {
+    println!("Received fen: {}", fen);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
+mod board;
+// use crate::board;
+
 use std::io;
+use scanner_rust::ScannerStr;
+
+fn initiate_bot() {
+    println!("initiating...");
+}
 
 fn main() {
-    let mut command;
-
     // Handling of UCI
 
     loop {
-        io::stdin().read_line(&mut command)
+        let mut message = String::new();
+
+        io::stdin().read_line(&mut message)
             .expect("Failed to read line");
+
+        let mut scanner = ScannerStr::new(&message);
+
+        let mut command = scanner.next().unwrap_or_default().unwrap_or_default();
         
-        match command.as_str() {
-            "ucinewgame" => println!("x"),
+        // match command.next().unwrap_or_default().unwrap_or_default() {
+        match command {
+            "ucinewgame" => initiate_bot(),
             "isready" => println!("readyok"),
-            "position" => println!("pos"),
+            "position" => board::read_fen(scanner.next().unwrap_or_default().unwrap_or_default()),
             "quit" => break,
-            _ => println!("unexpected command")
+            _ => println!("unexpected command"),
         }
     }
 }
