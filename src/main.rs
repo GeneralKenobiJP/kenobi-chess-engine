52adcd86fca0149c8d2f8f91c535cc509f17fd59
Jakub Pietrzak <kubapietrzak7@gmail.com>
2024-08-05 19:11:34 +0200

    Added basic UCI handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut command;
+
+    // Handling of UCI
+
+    loop {
+        io::stdin().read_line(&mut command)
+            .expect("Failed to read line");
+        
+        match command.as_str() {
+            "ucinewgame" => println!("x"),
+            "isready" => println!("readyok"),
+            "position" => println!("pos"),
+            "quit" => break,
+            _ => println!("unexpected command")
+        }
+    }
 }
