e34193750961dfafecef48b2246f1faf7e6bbe6d
Jakub Pietrzak <kubapietrzak7@gmail.com>
2024-08-10 16:20:08 +0200

    Added test start_position

--- src/move_generator.rs
+++ src/move_generator.rs
@@ -26,13 +26,11 @@ impl<'a> MoveList<'a> {
         }
     }
 
-    fn get_moves(&self) -> &'a Vec<Move> {
+    fn get_moves(&self) -> &Vec<Move> {
         &self.moves
     }
 
     fn generate_moves(&mut self) {
-        // let pawn_moves = if self.board.active_player == WHITE {self.generate_white_pawn_moves()}
-        //     else {self.generate_black_pawn_moves()};
         if self.board.active_player == WHITE
         {
             self.generate_white_pawn_moves();
@@ -49,9 +47,9 @@ impl<'a> MoveList<'a> {
         let double_push_bitboard = self.generate_white_double_push_bitboard(push_bitboard);
 
         let en_passant_tile = 1 << self.board.en_passant_possibility;
-        let left_capture_bitboard = self.generate_pawn_capture_bitboard(7, en_passant_tile);
+        let left_capture_bitboard = self.generate_pawn_capture_bitboard(7);
 
-        let right_capture_bitboard: u64 = self.generate_pawn_capture_bitboard(9, en_passant_tile);
+        let right_capture_bitboard: u64 = self.generate_pawn_capture_bitboard(9);
 
         self.convert_white_pawn_moves(push_bitboard, 8);
         self.convert_white_pawn_moves(double_push_bitboard, 16);
@@ -114,9 +112,9 @@ impl<'a> MoveList<'a> {
         let double_push_bitboard = self.generate_black_double_push_bitboard(push_bitboard);
 
         let en_passant_tile = 1 << self.board.en_passant_possibility;
-        let left_capture_bitboard = self.generate_pawn_capture_bitboard(-7, en_passant_tile);
+        let left_capture_bitboard = self.generate_pawn_capture_bitboard(-7);
 
-        let right_capture_bitboard: u64 = self.generate_pawn_capture_bitboard(-9, en_passant_tile);
+        let right_capture_bitboard: u64 = self.generate_pawn_capture_bitboard(-9);
 
         self.convert_black_pawn_moves(push_bitboard, 8);
         self.convert_black_pawn_moves(double_push_bitboard, 16);
@@ -173,10 +171,14 @@ impl<'a> MoveList<'a> {
 
     /// NEUTRAL
 
-    fn generate_pawn_capture_bitboard(&self, shift: i8, en_passant_tile: u64) -> u64 {
+    fn generate_pawn_capture_bitboard(&self, shift: i8) -> u64 {
+        let mut attack_options = self.board.colour_bitboards[self.board.inactive_player as usize];
+        if self.board.en_passant_possibility < 64 {
+            let en_passant_tile = 1 << self.board.en_passant_possibility;
+            attack_options |= en_passant_tile;
+        }
         let index = Piece::PAWN as usize;
-        let capture_bitboard: u64 = self.board.piece_bitboards[index] << shift
-            & (self.board.colour_bitboards[self.board.inactive_player as usize] + en_passant_tile);
+        let capture_bitboard: u64 = self.board.piece_bitboards[index] << shift & attack_options;
         capture_bitboard
     }
 }
@@ -187,14 +189,45 @@ mod tests {
     use super::*;
 
     #[test]
-    fn placeholder_name() {
+    fn start_position() {
         let mut board = Board::new();
         read_fen(&mut board, START_POSITION);
-        let move_list = MoveList::new(&board);
+        let mut move_list = MoveList::new(&board);
 
-        // assert_eq!(move_list.generate_white_pawn_moves(), 0b0000000000000000000000000000000011111111111111110000000000000000);
-        let x: u8 = 0b10000000;
-        x << 1;
-        assert_eq!(x, 0b10000000);
+        let mut expected_push_bitboard: u64 = 0b0000000000000000000000000000000000000000111111110000000000000000;
+        let mut expected_double_push_bitboard: u64 = 0b0000000000000000000000000000000011111111000000000000000000000000;
+        let mut expected_left_pawn_capture_bitboard: u64 = 0;
+        let mut expected_right_pawn_capture_bitboard: u64 = 0;
+
+        let mut expected_pawn_push = Vec::<Move>::new();
+        expected_pawn_push.push(Move{ origin: 8, target: 16, promotion: 0, piece: Piece::PAWN });
+        expected_pawn_push.push(Move{ origin: 9, target: 17, promotion: 0, piece: Piece::PAWN });
+        expected_pawn_push.push(Move{ origin: 10, target: 18, promotion: 0, piece: Piece::PAWN });
+        expected_pawn_push.push(Move{ origin: 11, target: 19, promotion: 0, piece: Piece::PAWN });
+        expected_pawn_push.push(Move{ origin: 12, target: 20, promotion: 0, piece: Piece::PAWN });
+        expected_pawn_push.push(Move{ origin: 13, target: 21, promotion: 0, piece: Piece::PAWN });
+        expected_pawn_push.push(Move{ origin: 14, target: 22, promotion: 0, piece: Piece::PAWN });
+        expected_pawn_push.push(Move{ origin: 15, target: 23, promotion: 0, piece: Piece::PAWN });
+
+        let mut expected_pawn_double_push = Vec::<Move>::new();
+        expected_pawn_push.push(Move{ origin: 8, target: 24, promotion: 0, piece: Piece::PAWN });
+        expected_pawn_push.push(Move{ origin: 9, target: 25, promotion: 0, piece: Piece::PAWN });
+        expected_pawn_push.push(Move{ origin: 10, target: 26, promotion: 0, piece: Piece::PAWN });
+        expected_pawn_push.push(Move{ origin: 11, target: 27, promotion: 0, piece: Piece::PAWN });
+        expected_pawn_push.push(Move{ origin: 12, target: 28, promotion: 0, piece: Piece::PAWN });
+        expected_pawn_push.push(Move{ origin: 13, target: 29, promotion: 0, piece: Piece::PAWN });
+        expected_pawn_push.push(Move{ origin: 14, target: 30, promotion: 0, piece: Piece::PAWN });
+        expected_pawn_push.push(Move{ origin: 15, target: 31, promotion: 0, piece: Piece::PAWN });
+
+        let mut expected_left_pawn_capture = Vec::<Move>::new();
+        let mut expected_right_pawn_capture = Vec::<Move>::new();
+
+        // move_list.generate_moves();
+        move_list.generate_white_push_bitboard();
+
+        assert_eq!(move_list.generate_white_push_bitboard(), expected_push_bitboard);
+        assert_eq!(move_list.generate_white_double_push_bitboard(expected_push_bitboard), expected_double_push_bitboard);
+        assert_eq!(move_list.generate_pawn_capture_bitboard(7), expected_left_pawn_capture_bitboard);
+        assert_eq!(move_list.generate_pawn_capture_bitboard(9), expected_right_pawn_capture_bitboard);
     }
 }
