75418dc6ca7714d9ebfd87ab0ef8448c1654f03a
Jakub Pietrzak <kubapietrzak7@gmail.com>
2024-08-22 19:30:15 +0200

    Tested quiet_move_bishop

--- src/board.rs
+++ src/board.rs
@@ -380,7 +380,38 @@ mod tests {
         assert_eq!(colour_bitboards[0] - origin + target, board.colour_bitboards[0]);
         assert_eq!(colour_bitboards[1], board.colour_bitboards[1]);
         for i in 0..12 {
-            if i == 1
+            if i == PAWN as usize
+            {
+                assert_eq!(piece_bitboards[i] - origin + target, board.piece_bitboards[i]);
+                continue;
+            }
+
+            assert_eq!(piece_bitboards[i], board.piece_bitboards[i]);
+        }
+    }
+
+    #[test]
+    fn quiet_move_bishop() {
+        let mut board = Board::new();
+        let fen = "r3k3/1Pr5/5pp1/3pPBPP/1b1P2Qq/2R2N2/P7/RK6 w q d6 1 25";
+        board.read_fen(fen);
+
+        let main_bitboard = board.main_bitboard;
+        let colour_bitboards = board.colour_bitboards.clone();
+        let piece_bitboards = board.piece_bitboards.clone();
+
+        let piece_move = Move { origin: 34, target: 43, promotion: 0, piece: BISHOP };
+
+        let origin: u64 = 1 << 34;
+        let target: u64 = 1 << 43;
+
+        board.make_move(&piece_move);
+
+        assert_eq!(main_bitboard - origin + target, board.main_bitboard);
+        assert_eq!(colour_bitboards[0] - origin + target, board.colour_bitboards[0]);
+        assert_eq!(colour_bitboards[1], board.colour_bitboards[1]);
+        for i in 0..12 {
+            if i == BISHOP as usize
             {
                 assert_eq!(piece_bitboards[i] - origin + target, board.piece_bitboards[i]);
                 continue;
@@ -422,7 +453,7 @@ mod tests {
     //             assert_eq!(piece_bitboards[i] - origin + target, board.piece_bitboards[i]);
     //             continue;
     //         }
-    // 
+    //
     //         assert_eq!(piece_bitboards[i], board.piece_bitboards[i]);
     //     }
     // }
