35758b2d5f4db5167bf42a278405b335fa134218
Jakub Pietrzak <kubapietrzak7@gmail.com>
2024-08-25 14:03:30 +0200

    Added perft_log

--- src/perft.rs
+++ src/perft.rs
@@ -26,6 +26,32 @@ pub fn perft(move_list: &mut MoveList, depth: u32) -> u64 {
     nodes
 }
 
+pub fn perft_log(move_list: &mut MoveList, depth: u32) -> u64 {
+    let mut nodes = 0u64;
+
+    move_list.generate_moves();
+
+    if depth == 1 {
+        // println!("No. of leaves: {}", move_list.get_moves().len());
+        // println!("Main bitboard: {}", move_list.get_board().main_bitboard);
+        // println!("Own bitboard: {}", move_list.get_board().colour_bitboards[move_list.get_board().active_player as usize]);
+        return move_list.get_moves().len() as u64;
+    }
+
+    let mut moves = move_list.get_moves().clone();
+
+    for piece_move in moves
+    {
+        move_list.make_move(&piece_move);
+        let descendants = perft(move_list, depth - 1);
+        println!("{}: {}", piece_move.to_algebraic_notation(), descendants);
+        nodes += descendants;
+        move_list.unmake_move(&piece_move);
+    }
+
+    nodes
+}
+
 // struct TreeStats {
 //     nodes: u64,
 //     captures: u64,
@@ -95,5 +121,29 @@ mod tests {
         let duration = start.elapsed();
         println!("perft {} returned {} nodes", depth, nodes);
         println!("bench_perft lasted for: {:?}", duration);
+    }
+
+    #[test]
+    fn perft_log_test() {
+        let mut board = Board::new();
+        board.read_fen(START_POSITION);
+        let mut move_list = MoveList::new(&mut board);
+
+        let depth = 3;
+
+        let nodes = perft_log(&mut move_list, depth);
+        println!("perft {} returned {} nodes", depth, nodes);
+    }
+
+    #[test]
+    fn perft_check() {
+        let mut board = Board::new();
+        board.read_fen("rnbqkbnr/pppppppp/8/8/8/P7/1PPPPPPP/RNBQKBNR b KQkq - 0 1");
+        let mut move_list = MoveList::new(&mut board);
+
+        let depth = 2;
+
+        let nodes = perft_log(&mut move_list, depth);
+        println!("perft {} returned {} nodes", depth, nodes);
     }
 }
