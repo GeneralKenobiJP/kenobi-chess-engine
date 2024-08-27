6dd1e4e896da7cb219deed229657a73fc82dfaf6
Jakub Pietrzak <kubapietrzak7@gmail.com>
2024-08-27 11:42:06 +0200

    Added count_material

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -18,7 +18,19 @@ pub fn evaluate(board: &Board) -> f32 {
 /// Counts the naive material difference on the board
 /// Outputs the difference between our material and opponent's material as i32
 fn count_material(board: &Board) -> i32 {
-    let material = 0;
+    let mut material = 0;
+
+    let active_player_piece_index = 6 * board.active_player as usize;
+    let inactive_player_piece_index = 6 * board.inactive_player as usize;
+
+    for piece in 0..6 {
+        material += count_pieces(board.piece_bitboards[active_player_piece_index]);
+        material -= count_pieces(board.piece_bitboards[inactive_player_piece_index]);
+    }
 
     material
+}
+
+fn count_pieces(piece_bitboard: u64) -> i32 {
+
 }
