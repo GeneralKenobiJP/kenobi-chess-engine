18bb73770d45518c138c399212fd0f011e73cabe
Jakub Pietrzak <kubapietrzak7@gmail.com>
2024-09-11 20:22:45 +0200

    Added new_game

--- src/bot.rs
+++ src/bot.rs
@@ -13,31 +13,37 @@ impl Bot {
         }
     }
 
-    pub fn message(message: &str) {
+    pub fn message(&mut self, message: &str) {
         let mut scanner = ScannerStr::new(message);
 
         let command = scanner.next().unwrap_or_default().unwrap_or_default();
         match command {
-            "uci" => uci(),
-            // "ucinewgame" => initiate_bot(&mut board),
-            // "isready" => println!("readyok"),
+            "uci" => Bot::uci(),
+            "ucinewgame" => self.new_game(),
+            "isready" => println!("readyok"),
             // "position" => board.read_fen(scanner.next().unwrap_or_default().unwrap_or_default()),
             // "quit" => break,
             _ => println!("unexpected command"),
+        }
     }
 
-        fn uci() {
-            id();
-            option();
-        }
+    fn uci() {
+        Bot::id();
+        Bot::option();
 
-        fn id() {
-            println!("id name Kenobi {}", env!("CARGO_PKG_VERSION"));
-            println!("id author Jakub Pietrzak");
-        }
+        println!("uciok");
+    }
 
-        fn option() {
-            
-        }
+    fn new_game(&mut self) {
+        self.engine = Engine::new();
+    }
+
+    fn id() {
+        println!("id name Kenobi {}", env!("CARGO_PKG_VERSION"));
+        println!("id author Jakub Pietrzak");
+    }
+
+    fn option() {
+
     }
 }
